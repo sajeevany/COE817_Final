091292bb1719250fcf3e4761a48fcc36304611f3
sajeevany <sajee@DESKTOP-H1525DA>
2016-04-07 11:56:25 -0400

    Singleton implementation for CLA

--- src/voteserver/CLA.java
+++ src/voteserver/CLA.java
@@ -2,4 +2,18 @@ package voteserver;
 
 public class CLA {
 
+	private static CLA myInstance = null;
+	private CLA(){}
+	
+	
+	public static CLA getInstance()
+	{
+		if (myInstance == null)
+		{
+			myInstance = new CLA();
+		}
+		
+		return myInstance;
+	}
+	
 }

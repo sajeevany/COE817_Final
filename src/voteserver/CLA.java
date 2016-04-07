625b6e8702e129f1caca99330f4d1c1cf469714f
sajeevany <sajee@DESKTOP-H1525DA>
2016-04-07 12:34:48 -0400

    Added listener to VoteServer

--- src/voteserver/CLA.java
+++ src/voteserver/CLA.java
@@ -1,5 +1,14 @@
 package voteserver;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class CLA {
 
 	private static CLA myInstance = null;
@@ -16,4 +25,56 @@ public class CLA {
 		return myInstance;
 	}
 	
+	private void startListening()
+	{
+		
+	}
+}
+
+class voteRequestListener
+{
+	@SuppressWarnings("unused")
+	private Runnable vRListener(final String host,final int myListeningPort)
+	{
+		
+		Runnable vrlRunnable = new Runnable()
+		{
+			ObjectInputStream dInStream = null;
+			Socket myListeningSocket;
+			VoteRequest recievedRequest;
+			
+			@Override
+			public void run() {
+							
+				try {
+					myListeningSocket = new Socket(host,myListeningPort);
+					dInStream = new ObjectInputStream(myListeningSocket.getInputStream());
+				} catch (IOException e1) {
+					e1.printStackTrace();
+					System.err.println("Unable to bind to socket at port " + myListeningPort + 
+							" and get input stream. Stopping VRListerner. Stopping application.");
+					System.exit(0);
+				}
+				
+				while (true)
+				{
+					try {
+						recievedRequest = (VoteRequest) dInStream.readObject();
+						
+						//test receiver
+						System.out.println(recievedRequest.toString());
+					} catch (ClassNotFoundException | IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+				
+			}
+			
+			
+		}; 
+		
+		return null;		
+	}
 }

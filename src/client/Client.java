c07e50f2a56a8dacd812136a5366493339b4648e
Krishna Deoram <krishna.deoram@gmail.com>
2016-04-20 12:12:58 -0400

    Sent stuff directly to CLA.

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,114 +1,105 @@
 package client;
 
+import encryption.JEncrypDES;
+import encryption.JEncryptRSA;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.concurrent.ArrayBlockingQueue;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.SecretKeySpec;
 
 import voteserver.CLA;
 import voteserver.VoteRequest;
 
 
-public class Client implements Runnable{
+public class Client// implements Runnable
+{
+    static String algorithm = "RSA";
+    static RSAPrivateKey privKey;
+    static PublicKey pubKey;
+    static String publicKeyString = "public_key_Server";
+   static String privateKeyString = "private_key_Client";
+   static String desAlgorithm = "DES";
+   static String keyString = "des_key";
+   static SecretKey secretKey;
+    
+    public static void main(String[] args) throws IOException {
+                            //1. Send a request to vote to the CLA.
+                            //3. Get the validation number from the CLA.
+                            //4. Cast vote to CTF
+                            //6. Read response from CTF. Close program.
+        //getDesKeysFromFiles();
+        getKeysFromFiles();
+        
+        byte[] fromCLA1 = CLA.start();
+        byte[] desKey = JEncryptRSA.decrypt(privKey, fromCLA1, algorithm);
+        secretKey = new SecretKeySpec(desKey, 0, desKey.length, desAlgorithm);
+        byte[] toCLA1 = JEncrypDES.encryptDES("Gumi is love. Gumi is life.", secretKey);
+        byte[] fromCLA2 = CLA.test2(toCLA1);
+        String fromCLAString = new String(JEncrypDES.decryptDES(fromCLA2, secretKey));
+        System.out.println(fromCLAString);
+    }
 	
-	public enum designation{server, client};
-	
-	private String hostToConnect;
-	private int port, myPublicKey, myPrivateKey, mySessionKey;
-	private Socket mySocket = null;
-	private String clientID, password, myVote;
-	private ObjectInputStream oInStream = null;
-	private ObjectOutputStream oOutStream = null;	
-	
+   private  static void getKeysFromFiles(){
+                    
+            //Get public key from file.
+       try {
+            FileInputStream keyfis = new FileInputStream(publicKeyString);
+            byte[] encKey = new byte[keyfis.available()];  
+            keyfis.read(encKey);
+            keyfis.close();
+            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encKey);
+            KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+            pubKey = keyFactory.generatePublic(pubKeySpec);
+            //System.out.println(pubKey);
+            
+            //Get private key from file.
+            FileInputStream keyfis2 = new FileInputStream(privateKeyString);
+            byte[] encKey2 = new byte[keyfis2.available()];  
+            keyfis2.read(encKey2);
+            keyfis2.close();
+            PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(encKey2);
+            KeyFactory keyFactory2 = KeyFactory.getInstance(algorithm);
+             privKey = (RSAPrivateKey)keyFactory2.generatePrivate(privKeySpec);
+            //System.out.println(privKey);
+       } catch (Exception e) {
+           System.out.println(e.toString());
+       }
+    }   
 
-	public Client (String hostToConnect, int portToConnect, String clientID, String password, String myVote)
-	{
-		if (hostToConnect == null || Integer.toString(portToConnect) == null)
-		{
-			throw new IllegalArgumentException("Input values are invalid");
-		}
-		this.hostToConnect = hostToConnect;
-		this.port = portToConnect;
-		this.clientID = clientID;
-		this.password = password;
-		this.myVote = myVote;
-
-	}
-	
-	public void run()
-	{		
-		try {
-				
-			System.out.println("[Client] Connecting to host:" + this.hostToConnect + " on port:"  + port);
-			mySocket =  new Socket(this.hostToConnect, this.port);
-			System.out.println("[Client] Connected established to host via " + mySocket.getLocalPort());
-		
-			
-			//setup streams - Output stream before input stream 
-			oOutStream = new ObjectOutputStream(new BufferedOutputStream(mySocket.getOutputStream()));
-			oInStream = new ObjectInputStream(new BufferedInputStream(mySocket.getInputStream()));
-						
-			//send vote request with credentials for validation
-			//initial validation number is unknown
-			VoteRequest vr = new VoteRequest(this.clientID, this.password, 1, VoteRequest.voteID.valueOf(this.myVote.toUpperCase()));
-			oOutStream.writeObject(vr);
-			oOutStream.flush();
-			
-			
-			closeApp();
-			
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/*
-	 * Close input/output streams before application termination
-	 */
-	private void closeApp()
-	{
-		try {
-			this.mySocket.close();
-			if (oInStream != null)
-				this.oInStream.close();
-			if (oOutStream != null)
-				this.oOutStream.close();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-			
-	}
-	
-	public static void main(String[] args) throws IOException
-	{
-		new Thread(CLA.getInstance()).start();
-		//new Thread(new Client("localhost", 10901, "Arjun")).start();
-		
-		BufferedReader clientListReader = new BufferedReader(new FileReader("clientList"));
-		String line = null;
-		
-		//Start clients
-		//element = (clientID, password, vote)
-		while((line = clientListReader.readLine())!=null)
-		{
-			StringTokenizer sTok = new StringTokenizer(line, ",");
-			new Thread(new Client("localhost", 10901, sTok.nextToken(), sTok.nextToken(), sTok.nextToken())).start();
-		}
-	
-		clientListReader.close();
-	}
-	
-
+    private static void getDesKeysFromFiles() { //Here just in case.
+        try {
+        FileInputStream keyfis = new FileInputStream(keyString);
+            byte[] encKey = new byte[keyfis.available()];  
+            keyfis.read(encKey);
+            keyfis.close();
+            SecretKeySpec keySpec = new SecretKeySpec(encKey, desAlgorithm);
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(desAlgorithm);
+            secretKey = keyFactory.generateSecret(keySpec);
+            //System.out.println(secretKey);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 }
